7ab5c62e96823cc83228c4419da055dbc836a4a0
uhooi <the_uhooi@yahoo.co.jp>
2022-11-10 16:01:43 +0900

    Refactor static

--- TotonoiPackage/Sources/Data/Sakatsu/Sakatsu.swift
+++ TotonoiPackage/Sources/Data/Sakatsu/Sakatsu.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct Sakatsu {
-    public static let empty: Self = .init(facilityName: "", visitingDate: .now, saunaSets: [.empty], comment: nil)
+    public static var empty: Self { .init(facilityName: "", visitingDate: .now, saunaSets: [.empty], comment: nil) }
     
     public var facilityName: String
     public var visitingDate: Date
@@ -25,11 +25,13 @@ extension Sakatsu: Codable {}
 
 #if DEBUG
 extension Sakatsu {
-    public static let preview: Self = .init(
-        facilityName: "サウナウホーイ",
-        visitingDate: .now,
-        saunaSets: [.preview, .preview, .preview],
-        comment: "コメントテスト"
-    )
+    public static var preview: Self {
+        .init(
+            facilityName: "サウナウホーイ",
+            visitingDate: .now,
+            saunaSets: [.preview, .preview, .preview],
+            comment: "コメントテスト"
+        )
+    }
 }
 #endif
