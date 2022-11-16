d4a0a123a17e6f896cf85d2c5394bed7fc0f203a
uhooi <the_uhooi@yahoo.co.jp>
2022-11-16 10:55:23 +0900

    Refactor UserDefaultsClient

--- TotonoiPackage/Sources/Data/Sakatsu/SakatsuRepository.swift
+++ TotonoiPackage/Sources/Data/Sakatsu/SakatsuRepository.swift
@@ -9,15 +9,17 @@ public struct SakatsuUserDefaultsClient {
     public static let shared: Self = .init()
     private static let sakatsusKey = "sakatsus"
     
+    private let userDefaultsClient = UserDefaultsClient.shared
+    
     private init() {}
 }
 
 extension SakatsuUserDefaultsClient: SakatsuRepository {
     public func sakatsus() throws -> [Sakatsu] {
-        try UserDefaultsClient.shared.object(forKey: Self.sakatsusKey)
+        try userDefaultsClient.object(forKey: Self.sakatsusKey)
     }
     
     public func saveSakatsus(_ sakatsus: [Sakatsu]) throws {
-        try UserDefaultsClient.shared.setObject(sakatsus, forKey: Self.sakatsusKey)
+        try userDefaultsClient.setObject(sakatsus, forKey: Self.sakatsusKey)
     }
 }
