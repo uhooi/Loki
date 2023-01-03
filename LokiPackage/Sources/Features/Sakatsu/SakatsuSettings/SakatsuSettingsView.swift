6271c3b27373f4158ea80951626ba65e615e6020
uhooi <the_uhooi@yahoo.co.jp>
2023-01-03 15:56:09 +0900

    Add default times input

--- LokiPackage/Sources/Features/Sakatsu/SakatsuSettings/SakatsuSettingsView.swift
+++ LokiPackage/Sources/Features/Sakatsu/SakatsuSettings/SakatsuSettingsView.swift
@@ -1,9 +1,36 @@
 import SwiftUI
+import SakatsuData
 
 struct SakatsuSettingsView: View {
+    let defaultSaunaSet: SaunaSet
+    
+    let onDefaultSaunaTimeChange: ((TimeInterval?) -> Void)
+    let onDefaultCoolBathTimeChange: ((TimeInterval?) -> Void)
+    let onDefaultRelaxationTimeChange: ((TimeInterval?) -> Void)
+    
     var body: some View {
         Form {
+            defaultTimesSection
             versionSection
+        }
+    }
+    
+    private var defaultTimesSection: some View {
+        Section {
+            saunaSetItemTimeInputView(
+                saunaSetItem: defaultSaunaSet.sauna,
+                onTimeChange: onDefaultSaunaTimeChange
+            )
+            saunaSetItemTimeInputView(
+                saunaSetItem: defaultSaunaSet.coolBath,
+                onTimeChange: onDefaultCoolBathTimeChange
+            )
+            saunaSetItemTimeInputView(
+                saunaSetItem: defaultSaunaSet.relaxation,
+                onTimeChange: onDefaultRelaxationTimeChange
+            )
+        } header: {
+            Text("Default times", bundle: .module)
         }
     }
     
@@ -18,12 +45,34 @@ struct SakatsuSettingsView: View {
             Text("© 2023 THE Uhooi")
         }
     }
+    
+    private func saunaSetItemTimeInputView(
+        saunaSetItem: any SaunaSetItemProtocol,
+        onTimeChange: @escaping (TimeInterval?) -> Void
+    ) -> some View {
+        HStack {
+            Text(saunaSetItem.emoji + saunaSetItem.title)
+            TextField(String(localized: "Optional", bundle: .module), value: .init(get: {
+                saunaSetItem.time
+            }, set: { newValue in
+                onTimeChange(newValue)
+            }), format: .number)
+            .keyboardType(.decimalPad)
+            .multilineTextAlignment(.trailing)
+            Text(saunaSetItem.unit)
+        }
+    }
 }
 
 #if DEBUG
 struct SakatsuSettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SakatsuSettingsView()
+        SakatsuSettingsView(
+            defaultSaunaSet: .preview,
+            onDefaultSaunaTimeChange: { _ in },
+            onDefaultCoolBathTimeChange: { _ in },
+            onDefaultRelaxationTimeChange: { _ in }
+        )
     }
 }
 #endif
