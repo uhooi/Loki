4b2c5427e504b1acca7f9e36d179fc639babaecd
uhooi <the_uhooi@yahoo.co.jp>
2023-03-09 23:56:44 +0900

    Fix production

--- App/Loki/Production/ProductionApp.swift
+++ App/Loki/Production/ProductionApp.swift
@@ -5,7 +5,7 @@ import ProductionApp
 struct ProductionApp: App {
     var body: some Scene {
         WindowGroup {
-            ProductionRouter.shared.firstScreen()
+            ProductionRootScreen()
         }
     }
 }
