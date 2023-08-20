cfda750f74c548f03c7172d2fb3fd3fd872946ec
uhooi <the_uhooi@yahoo.co.jp>
2023-08-20 22:34:19 +0900

    Remove DefaultUserDefaultsClient.swift

--- LokiPackage/Sources/Core/UserDefaults/UserDefaultsClient.swift
+++ LokiPackage/Sources/Core/UserDefaults/UserDefaultsClient.swift
@@ -1,4 +1,32 @@
+import Foundation
+
 public protocol UserDefaultsClient {
     func object<V: Decodable>(forKey key: UserDefaultsKey) throws -> V
     func set<V: Encodable>(_ value: V, forKey key: UserDefaultsKey) throws
+}
+
+public final class DefaultUserDefaultsClient {
+    public static let shared = DefaultUserDefaultsClient()
+
+    private let userDefaults = UserDefaults.standard
+
+    private init() {}
+}
+
+extension DefaultUserDefaultsClient: UserDefaultsClient {
+    public func object<V: Decodable>(forKey key: UserDefaultsKey) throws -> V {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        guard let data = userDefaults.data(forKey: key.rawValue) else {
+            throw UserDefaultsError.missingValue(key: key)
+        }
+        return try jsonDecoder.decode(V.self, from: data)
+    }
+
+    public func set<V: Encodable>(_ value: V, forKey key: UserDefaultsKey) throws {
+        let jsonEncoder = JSONEncoder()
+        jsonEncoder.keyEncodingStrategy = .convertToSnakeCase
+        let data = try jsonEncoder.encode(value)
+        userDefaults.set(data, forKey: key.rawValue)
+    }
 }
