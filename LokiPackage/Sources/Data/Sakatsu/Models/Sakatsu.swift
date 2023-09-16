8a81e81779d5e803f32eac2e90b35e9c2fd84e16
uhooi <the_uhooi@yahoo.co.jp>
2023-09-16 15:53:34 +0900

    Fix ForEach

--- LokiPackage/Sources/Data/Sakatsu/Models/Sakatsu.swift
+++ LokiPackage/Sources/Data/Sakatsu/Models/Sakatsu.swift
@@ -16,6 +16,8 @@ public struct Sakatsu: Identifiable {
     }
 }
 
+extension Sakatsu: Hashable {}
+
 extension Sakatsu: Equatable {
     public static func == (lhs: Sakatsu, rhs: Sakatsu) -> Bool {
         lhs.id == rhs.id
